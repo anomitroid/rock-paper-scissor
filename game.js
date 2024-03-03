e57f531e27aca942b1b54526220be2460271f648
anomitroid <anomitroid2005@gmail.com>
2024-03-03 22:05:47 +0530

    updated ui

--- game.js
+++ game.js
@@ -7,26 +7,55 @@ function getComputerChoice () {
 let playerScore = 0;
 let computerScore = 0;
 
+const container = document.createElement ('div');
+document.documentElement.appendChild (container);
+const rock = document.createElement ('button');
+const paper = document.createElement ('button');
+const scissor = document.createElement ('button');
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissor.textContent = "Scissor";
+container.appendChild (rock);
+container.appendChild (paper);
+container.appendChild (scissor);
+
+rock.addEventListener ('click', () => playRound("Rock", getComputerChoice()));
+paper.addEventListener ('click', () => playRound("Paper", getComputerChoice()));
+scissor.addEventListener ('click', () => playRound("Scissor", getComputerChoice()));
+
+const resultBox = document.createElement ('div');
+container.appendChild (resultBox);
+
+const scoreBox = document.createElement ('div');
+const playerScoreBox = document.createElement ('p');
+const computerScoreBox = document.createElement ('p');
+scoreBox.appendChild (playerScoreBox);
+scoreBox.appendChild (computerScoreBox);
+container.appendChild (scoreBox);
+
 function playRound (playerSelection, computerSelection) {
     const player = playerSelection.toLowerCase();
     const computer = computerSelection.toLowerCase();
-    if (player === computer) return "It's a Tie!";
+    let roundResult = "";
+    if (player === computer) roundResult = "It's a Tie!";
     else if ((player === 'rock' && computer === 'scissor') || (player === 'paper' && computer === 'rock') || player === 'scissor' && computer === 'paper') {
         playerScore ++;
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        roundResult = `You Win! ${playerSelection} beats ${computerSelection}`;
     }
     else {
         computerScore ++;
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        roundResult = `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    resultBox.textContent = roundResult;
+    playerScoreBox.textContent = `Player: ${playerScore}`;
+    computerScoreBox.textContent = `Computer: ${computerScore}`;
+
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore == 5) resultBox.textContent = "Congrats, you WON!!!";
+        else resultBox.textContent = "You Lost, Better Luck Next Time";
+        playerScore = 0; computerScore = 0;
+        playerScoreBox.textContent = `Player: ${playerScore}`;
+        computerScoreBox.textContent = `Computer: ${computerScore}`;
+        document.querySelectorAll('button').disabled = true;
     }
 }
-
-for (let i = 0; i < 5; i ++) {
-    const playerSelection = prompt ("Enter your move!");
-    const computerSelection = getComputerChoice ();
-    console.log (playRound (playerSelection, computerSelection));
-}
-
-if (playerScore > computerScore) console.log("Yay! You win.");
-else if (score == computerScore) console.log("Wow, It's a tie");
-else console.log ("You Lose! Better luck next time.");
